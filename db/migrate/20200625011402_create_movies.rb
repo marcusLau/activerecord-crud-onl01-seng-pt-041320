01d1a5877d9b74e633f4eea223bb616cc26ea78f
marcusLau <lau.marcus76@gmail.com>
2020-06-24 19:08:43 -0700

    Done.

---
+++ db/migrate/20200625011402_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |i|
+      i.string :title
+      i.integer :release_date
+      i.string :director
+      i.string :lead
+      i.boolean :in_theaters
+    end
+  end
+end
