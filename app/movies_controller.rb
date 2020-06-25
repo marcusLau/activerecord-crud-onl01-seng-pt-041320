bb93e474844eb776dc5cd1fa8f8b98bf88bb8524
marcusLau <lau.marcus76@gmail.com>
2020-06-24 21:51:41 -0700

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -71,16 +71,18 @@ end
 def can_be_found_updated_and_saved
   # Update the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
-  # movie = Movie.find_by(title: "Awesome Flick")
-  # movie.title = "Evern Awesomer Flick"
-  # movie
+  movie = Movie.find_by(title: "Awesome Flick")
+  movie.title = "Even Awesomer Flick"
+  movie.save
+  movie
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  movie = Movie.find_by(title: "Wat?")
+  movie.update(title: "Wat, huh?")
+  movie.save
 end
 
 def can_update_multiple_items_at_once
@@ -88,18 +90,20 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  5.times do |j|
+    Movie.update(title: "A Movie")
+  end
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  movie.destroy # deletes item from Movie.
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.destroy_all
 end
